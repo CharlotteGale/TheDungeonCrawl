b77af03399f864d3568b1280817ae50dc69de878
Charlotte Gale <CharlotteMGale@outlook.com>
2026-03-11 16:43:58 +0000

    adds methods to the Player class

--- src/main/kotlin/Player.kt
+++ src/main/kotlin/Player.kt
@@ -1,4 +1,21 @@
 package org.example
 
-class Player(var currentRoomId: String, val inventory: MutableList<Item>) {
+class Player(var currentRoomId: String, val inventory: MutableList<Item> = mutableListOf()) {
+    fun takeItem(item: Item) {
+        inventory.add(item)
+    }
+
+    fun dropItem(itemId: String) : Item? {
+        val item = inventory.find { it.id == itemId }
+        return if (item != null) {
+            inventory.remove(item)
+            item
+        } else {
+            null
+        }
+    }
+
+    fun hasItem(itemId: String) :Boolean {
+        return inventory.any { it.id == itemId }
+    }
 }
